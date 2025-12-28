825b64f214c92daa08ec61a3deee9b613a02b56c
3llips1s <kiurire.gichia@gmail.com>
2025-12-28 18:04:00 +0300

    fixed fullscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
 
 import 'features/game/logic/flappy_bart_game.dart';
 import 'features/nouns/data/csv_loader.dart';
@@ -8,20 +9,13 @@ import 'features/nouns/data/csv_loader.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+
+  if (!kIsWeb) {
+    await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  }
+
   await CsvLoader.loadNouns();
-
-  await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light,
-
-      systemNavigationBarColor: Colors.transparent,
-      systemStatusBarContrastEnforced: false,
-      systemNavigationBarDividerColor: Colors.transparent,
-    ),
-  );
 
   runApp(const MainApp());
 }
@@ -31,74 +25,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: GameScreen());
-  }
-}
-
-class GameScreen extends StatefulWidget {
-  const GameScreen({super.key});
-
-  @override
-  State<GameScreen> createState() => _GameScreenState();
-}
-
-class _GameScreenState extends State<GameScreen> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-
-    _enforceFullScreen();
-
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _enforceFullScreen();
-    });
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    if (state == AppLifecycleState.resumed) {
-      _enforceFullScreen();
-    }
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    _enforceFullScreen();
-  }
-
-  Future<void> _enforceFullScreen() async {
-    await SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-
-    SystemChrome.setSystemUIOverlayStyle(
-      const SystemUiOverlayStyle(
-        statusBarColor: Colors.transparent,
-        statusBarIconBrightness: Brightness.light,
-
-        systemNavigationBarColor: Colors.transparent,
-        systemNavigationBarDividerColor: Colors.transparent,
-        systemStatusBarContrastEnforced: false,
-      ),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: GameScreen(),
     );
   }
+}
+
+class GameScreen extends StatelessWidget {
+  const GameScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       extendBody: true,
       extendBodyBehindAppBar: true,
-      body: SafeArea(
-        top: false,
-        bottom: false,
-        child: GameWidget(game: FlappyBartGame()),
-      ),
+      backgroundColor: Colors.transparent,
+      body: GameWidget(game: FlappyBartGame()),
     );
   }
 }
